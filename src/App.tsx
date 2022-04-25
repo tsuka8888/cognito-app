c91fefab0f972a2451c80ded15e17d29e1bb3b35
yamamoto.tsukasa <yamamoto.tsukasa@ditgroup.jp>
2022-04-25 14:23:33 +0900

    ログイン画面の日本語化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Amplify } from "aws-amplify";
+import { Amplify, I18n } from "aws-amplify";
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 import awsExports from "./aws-exports";
+import { vocabularies } from "./vocablaries";
 Amplify.configure(awsExports);
+
+I18n.putVocabularies(vocabularies);
+I18n.setLanguage('ja');
 
 export default function App() {
   return (
